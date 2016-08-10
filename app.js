2018d6ba3a14eb563f4205908cf12b5d35b15a68
Dion MacIntyre <dion@macintyres.me.uk>
2016-08-10 13:42:12 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,69 @@
+﻿'use strict';
+
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const config = require('./config');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const songs = require('./routes/songs');
+const playlists = require('./routes/playlists');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(3700, function () {
+    console.log('App listening on port 3700!');
+});
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,3 @@
+﻿module.exports = {
+    database: 'mongodb://dbaccess:2UQ8pYeeVe@ds153715.mlab.com:53715/lstnr'
+}
